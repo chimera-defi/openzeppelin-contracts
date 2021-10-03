d7088ddafbb88975382ed9b1701a0b9259bef76a
Chimera <81845287+chimera-defi@users.noreply.github.com>
2021-10-03 14:49:10 +0530

    fix path

--- contracts/utils/Address.sol
+++ contracts/utils/Address.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity >=0.6.2 <0.8.0;
-import "../ERC20/IERC20.sol";
+import "../token/ERC20/IERC20.sol";
 
 /**
  * @dev Collection of functions related to the address type
